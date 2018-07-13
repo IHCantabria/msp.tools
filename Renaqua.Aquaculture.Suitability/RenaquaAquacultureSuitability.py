f2349bb0bbcb4dc6cf6ae2198334c9051765ebc8
Iñaki Silanes <isilanes@gmail.com>
2018-07-13 11:33:23 +0200

    implement reading conf

--- Renaqua.Aquaculture.Suitability/RenaquaAquacultureSuitability.py
+++ Renaqua.Aquaculture.Suitability/RenaquaAquacultureSuitability.py
@@ -1,10 +1,24 @@
+# Standard libs:
+import json
+
 # Our libs:
 from libaquaculture import ihdata
 
 
+# Globals:
+CONF_FILE = "conf/RenaquaAquacultureSuitability.json"
+
+
 # Functions:
-def main():    
-    pass
+def main():
+    with open(CONF_FILE) as f:
+        J = json.load(f)
+
+    print(J)
+    # qué poner aquí para que imprima la URL?
+    #print(thredds_base_url)
+
+    #descargar(conf)
 
 
 # Main body:
