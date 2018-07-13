7b8e7adaec871de86d79779398a0eb7106df63a4
Iñaki Silanes <isilanes@gmail.com>
2018-07-13 11:00:53 +0200

    minor cosmetic changes

---
+++ Renaqua.Aquaculture.Suitability/RenaquaAquacultureSuitability.py
@@ -0,0 +1,12 @@
+# Our libs:
+from libaquaculture import ihdata
+
+
+# Functions:
+def main():    
+    pass
+
+
+# Main body:
+if __name__ == '__main__':
+    main()
---
+++ Renaqua.Aquaculture.Suitability/libaquaculture/ihdata.py
@@ -0,0 +1,10 @@
+# Classes:
+class IHData(object):
+    """Class to manage access to 'IHData' netCDFs."""
+
+    def __init__(self):
+        pass  # Empty constructor
+    
+    def method_name(self, parameter_list):
+        pass
+
