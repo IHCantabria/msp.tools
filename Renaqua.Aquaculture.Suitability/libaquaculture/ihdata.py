bddffe89d36bd0d496bc58f02f0ccb1767cd9460
Iñaki Silanes <isilanes@gmail.com>
2018-07-27 09:55:13 +0200

    jugar con slicing de variable

--- Renaqua.Aquaculture.Suitability/libaquaculture/ihdata.py
+++ Renaqua.Aquaculture.Suitability/libaquaculture/ihdata.py
@@ -12,10 +12,24 @@ class IHData(object):
     def get_variable(self, var_name, conf):
         """Returns contents of variable 'var_name'."""
 
+        fn = "http://test.opendap.org/dap/data/nc/coads_climatology.nc"
+        fn = "ARMOR3D_REPv3-1_20130102_20140420.nc"
+
         fn = conf["url_file"]
 
         with nc4.Dataset(fn) as nc:
-            var = nc.variables.keys()
+            print(nc.variables.keys())
+            var = nc.variables["salinity"][0:5, 0:5, 0:5]
 
-        return var
+            print(var)
+
+            return None
+
+            #var = nc.variables["salinity"][0, 0, 0]
+            #var = var[0, :2, :2]
+            #print(var)
+
+        return None
+        print("abc")
+        print("aaa")
 
