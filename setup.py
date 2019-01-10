1eebba2a655419612588ed818b2020206698a0be
Luis Pedraz <LPedraz1981@gmail.com>
2019-01-10 14:52:30 +0100

    Updated version to 0.6

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_namespace_packages
 
+
 def readme():
     with open('README.md') as f:
         return f.read()
+
+
 setup(name='msptools',
-      version='0.5',
+      version='0.6',
       description='Marine Spatial Planning: Aquaculture',
       url='https://github.com/ihcantabria/msptools',
       author='Felipe Maza',
