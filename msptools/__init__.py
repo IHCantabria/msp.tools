fccba00a564bf68378631f8907e1311e2b432441
Luis Pedraz <LPedraz1981@gmail.com>
2019-01-10 12:26:29 +0100

    Set access point for historical serie

--- msptools/__init__.py
+++ msptools/__init__.py
@@ -1,3 +1,9 @@
 from . import aquaculture
+
+
 def run_biological(params):
     return aquaculture.run_biological(params)
+
+
+def load_historical_serie(params):
+    return aquaculture.load_historical_serie(params)
