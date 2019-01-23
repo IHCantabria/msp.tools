0ab4603556c03f0204ff713c37eec332ea11da04
Luis Pedraz <LPedraz1981@gmail.com>
2019-01-23 11:25:36 +0100

    vesrion 0.8

--- tests/test_data.py
+++ tests/test_data.py
@@ -35,20 +35,40 @@ class Test_Data(unittest.TestCase):
 
     def test_scale_factor(self):
         expected_value = 18.6481216738  # temp on 2/1/2015(day/month/year)
+        expected_salinity = 36.597797192  # salinity on 2/1/2015
         thredds = Thredds(CONFIG["copernicus"]
                           ["global_reanalysis_physical"]["catalog"])
         raw_value = -3211
+        raw_salinity = 23985
 
         real_value = thredds.get_real_value(raw_value, 7.324442E-4, 21)
+        real_salinity = thredds.get_real_value(
+            raw_salinity, 0.0015259255, -0.0015259255)
         self.assertAlmostEqual(real_value, expected_value)
+        self.assertAlmostEqual(real_salinity, expected_salinity)
 
     def test_check_output_format(self):
-        timestamp = 1420200000.0  # 2/1/2015
-        expected_value = 18.6481216738  # temp on 2/1/2015(day/month/year)
         data = aquaculture.load_historical_serie(self.params)
         self.assertIsNotNone(data)
-        self.assertEqual(len(data['serie']), 60)
-        self.assertIsInstance(data['serie'][timestamp]['date'], datetime.date)
-        self.assertEqual(len(data['serie'][timestamp]['measures']), 2)
-        self.assertEqual(data['serie'][timestamp]['measures']
-                         [0]['value'], expected_value)
+        for measured_variable in data['measures']:
+            self.assertIsNotNone(measured_variable['var_alias'])
+            self.assertIsNotNone(measured_variable['var_name'])
+            self.assertIsNotNone(measured_variable['units'])
+            self.assertIsNotNone(measured_variable['values'])
+            self.assertIsInstance(measured_variable['values'], list)
+
+    def test_values_on_day(self):
+        timestamp = 1420200000.0  # 2/1/2015
+        expected_temperature = 18.6481216738  # temp on 2/1/2015
+        expected_salinity = 36.597797192  # salinity on 2/1/2015
+        data = aquaculture.load_historical_serie(self.params)
+        self.assertEqual(len(data['measures'][0]['values']), 60)
+        self.assertEqual(len(data['measures'][1]['values']), 60)
+        # Check temperature
+        for value in data['measures'][0]['values']:
+            if (value[0] == timestamp):
+                self.assertEqual(value[1], expected_temperature)
+        # Check salinity
+        for value in data['measures'][1]['values']:
+            if (value[0] == timestamp):
+                self.assertEqual(value[1], expected_salinity)
